33ee0e237fa121fb06f15347f90ba968599139d9
Klaci00 <kenyolaszlo6@gmail.com>
2026-03-23 20:48:25 +0100

    Implement temporary card lock feature in check_card_person function.

--- django-rest-server/entry_validation/card_person_check/check.py
+++ django-rest-server/entry_validation/card_person_check/check.py
@@ -19,7 +19,9 @@ def check_card_person(card : Card | None, employee : Employee | None) -> Respons
     if card.lock_until and card.lock_until > timezone.now():
         return Response({"denied": "Card is temporarily locked."}, status=403)
     
-    if random.randint(0, 99) == 1:
+    if random.randint(0, 1) == 1:
+        card.lock_until = timezone.now() + timezone.timedelta(minutes=5)
+        card.save()
         return Response({"denied" : {"control" : True}}, status=403)
     
     return Response({"message": "Card and person details are valid."}, status=200)
